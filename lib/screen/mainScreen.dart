59ea7f9c008f96fd4c51953e397a4fce0476f12f
Huiwon <71807924+hu1won@users.noreply.github.com>
2022-12-19 19:26:23 +0900

    [Add] navigation setting

---
+++ lib/screen/mainScreen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Main Screen'),
+      ),
+      body: const Center(
+        child: Text('Main Screen'),
+      ),
+    );
+  }
+}
